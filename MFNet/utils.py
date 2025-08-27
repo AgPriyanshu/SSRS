39c871daebc6c7dff5da4d7d07e16ec216b3d92b
Priyanshu Agarwal <priyanshu81212@gmail.com>
2025-08-27 14:50:33 +0530

    Basic Cleanup

--- MFNet/utils.py
+++ MFNet/utils.py
@@ -8,74 +8,105 @@ from torchvision.utils import make_grid
 from PIL import Image
 from skimage import io
 import os
+from constants import N_CLASSES
 
-# Parameters
-## SwinFusion
-WINDOW_SIZE = (256, 256) # Patch size
-
-STRIDE = 32 # Stride for testing
-IN_CHANNELS = 3 # Number of input channels (e.g. RGB)
-FOLDER = "/media/lscsc/nas/xianping/ISPRS_dataset/" # Replace with your "/path/to/the/ISPRS/dataset/folder/"
-BATCH_SIZE = 10 
-# BATCH_SIZE = 4 # For backbone ViT-Huge
-
-LABELS = ["roads", "buildings", "low veg.", "trees", "cars", "clutter"] # Label names
-N_CLASSES = len(LABELS) # Number of classes
-WEIGHTS = torch.ones(N_CLASSES) # Weights for class balancing
-CACHE = True # Store the dataset in-memory
+WEIGHTS = torch.ones(N_CLASSES)  # Weights for class balancing
 
 # ISPRS color palette
 # Let's define the standard ISPRS color palette
-palette = {0 : (255, 255, 255), # Impervious surfaces (white)
-           1 : (0, 0, 255),     # Buildings (blue)
-           2 : (0, 255, 255),   # Low vegetation (cyan)
-           3 : (0, 255, 0),     # Trees (green)
-           4 : (255, 255, 0),   # Cars (yellow)
-           5 : (255, 0, 0),     # Clutter (red)
-           6 : (0, 0, 0)}       # Undefined (black)
+palette = {
+    0: (255, 255, 255),  # Impervious surfaces (white)
+    1: (0, 0, 255),  # Buildings (blue)
+    2: (0, 255, 255),  # Low vegetation (cyan)
+    3: (0, 255, 0),  # Trees (green)
+    4: (255, 255, 0),  # Cars (yellow)
+    5: (255, 0, 0),  # Clutter (red)
+    6: (0, 0, 0),
+}  # Undefined (black)
 
 invert_palette = {v: k for k, v in palette.items()}
 
-MODEL = 'UNetformer'
+MODEL = "UNetformer"
 # MODEL = 'FTUNetformer'
-MODE = 'Train'
+MODE = "Train"
 # MODE = 'Test'
-DATASET = 'Vaihingen'
+DATASET = "Vaihingen"
 # DATASET = 'Potsdam'
 IF_SAM = True
 # IF_SAM = False
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-if DATASET == 'Vaihingen':
-    train_ids = ['1', '3', '23', '26', '7', '11', '13', '28', '17', '32', '34', '37']
-    test_ids = ['5', '21', '15', '30']
+if DATASET == "Vaihingen":
+    train_ids = ["1", "3", "23", "26", "7", "11", "13", "28", "17", "32", "34", "37"]
+    test_ids = ["5", "21", "15", "30"]
     Stride_Size = 32
     epochs = 50
     save_epoch = 1
-    MAIN_FOLDER = FOLDER + 'Vaihingen/'
-    DATA_FOLDER = MAIN_FOLDER + 'top/top_mosaic_09cm_area{}.tif'
-    DSM_FOLDER = MAIN_FOLDER + 'dsm/dsm_09cm_matching_area{}.tif'
-    LABEL_FOLDER = MAIN_FOLDER + 'gts_for_participants/top_mosaic_09cm_area{}.tif'
-    ERODED_FOLDER = MAIN_FOLDER + 'gts_eroded_for_participants/top_mosaic_09cm_area{}_noBoundary.tif'
-elif DATASET == 'Potsdam':
-    train_ids = ['6_10', '7_10', '2_12', '3_11', '2_10', '7_8', '5_10', '3_12', '5_12', '7_11', '7_9', '6_9', '7_7',
-                '4_12', '6_8', '6_12', '6_7', '4_11']
-    test_ids = ['4_10', '5_11', '2_11', '3_10', '6_11', '7_12']
+    MAIN_FOLDER = FOLDER + "Vaihingen/"
+    DATA_FOLDER = MAIN_FOLDER + "top/top_mosaic_09cm_area{}.tif"
+    DSM_FOLDER = MAIN_FOLDER + "dsm/dsm_09cm_matching_area{}.tif"
+    LABEL_FOLDER = MAIN_FOLDER + "gts_for_participants/top_mosaic_09cm_area{}.tif"
+    ERODED_FOLDER = (
+        MAIN_FOLDER
+        + "gts_eroded_for_participants/top_mosaic_09cm_area{}_noBoundary.tif"
+    )
+elif DATASET == "Potsdam":
+    train_ids = [
+        "6_10",
+        "7_10",
+        "2_12",
+        "3_11",
+        "2_10",
+        "7_8",
+        "5_10",
+        "3_12",
+        "5_12",
+        "7_11",
+        "7_9",
+        "6_9",
+        "7_7",
+        "4_12",
+        "6_8",
+        "6_12",
+        "6_7",
+        "4_11",
+    ]
+    test_ids = ["4_10", "5_11", "2_11", "3_10", "6_11", "7_12"]
     Stride_Size = 128
     epochs = 50
     save_epoch = 1
-    MAIN_FOLDER = FOLDER + 'Potsdam/'
-    DATA_FOLDER = MAIN_FOLDER + '4_Ortho_RGBIR/top_potsdam_{}_RGBIR.tif'
-    DSM_FOLDER = MAIN_FOLDER + '1_DSM_normalisation/dsm_potsdam_{}_normalized_lastools.jpg'
-    LABEL_FOLDER = MAIN_FOLDER + '5_Labels_for_participants/top_potsdam_{}_label.tif'
-    ERODED_FOLDER = MAIN_FOLDER + '5_Labels_for_participants_no_Boundary/top_potsdam_{}_label_noBoundary.tif'
+    MAIN_FOLDER = FOLDER + "Potsdam/"
+    DATA_FOLDER = MAIN_FOLDER + "4_Ortho_RGBIR/top_potsdam_{}_RGBIR.tif"
+    DSM_FOLDER = (
+        MAIN_FOLDER + "1_DSM_normalisation/dsm_potsdam_{}_normalized_lastools.jpg"
+    )
+    LABEL_FOLDER = MAIN_FOLDER + "5_Labels_for_participants/top_potsdam_{}_label.tif"
+    ERODED_FOLDER = (
+        MAIN_FOLDER
+        + "5_Labels_for_participants_no_Boundary/top_potsdam_{}_label_noBoundary.tif"
+    )
 
-print(MODEL + ', ' + MODE + ', ' + DATASET + ', IF_SAM: ' + str(IF_SAM) + ', WINDOW_SIZE: ', WINDOW_SIZE, 
-      ', BATCH_SIZE: ' + str(BATCH_SIZE), ', Stride_Size: ', str(Stride_Size),
-      ', epochs: ' + str(epochs), ', save_epoch: ', str(save_epoch),)
+print(
+    MODEL
+    + ", "
+    + MODE
+    + ", "
+    + DATASET
+    + ", IF_SAM: "
+    + str(IF_SAM)
+    + ", WINDOW_SIZE: ",
+    WINDOW_SIZE,
+    ", BATCH_SIZE: " + str(BATCH_SIZE),
+    ", Stride_Size: ",
+    str(Stride_Size),
+    ", epochs: " + str(epochs),
+    ", save_epoch: ",
+    str(save_epoch),
+)
+
 
 def convert_to_color(arr_2d, palette=palette):
-    """ Numeric labels to RGB-color encoding """
+    """Numeric labels to RGB-color encoding"""
     arr_3d = np.zeros((arr_2d.shape[0], arr_2d.shape[1], 3), dtype=np.uint8)
 
     for c, i in palette.items():
@@ -84,8 +115,9 @@ def convert_to_color(arr_2d, palette=palette):
 
     return arr_3d
 
+
 def convert_from_color(arr_3d, palette=invert_palette):
-    """ RGB-color encoding to grayscale labels """
+    """RGB-color encoding to grayscale labels"""
     arr_2d = np.zeros((arr_3d.shape[0], arr_3d.shape[1]), dtype=np.uint8)
 
     for c, i in palette.items():
@@ -94,15 +126,25 @@ def convert_from_color(arr_3d, palette=invert_palette):
 
     return arr_2d
 
+
 def save_img(tensor, name):
-    tensor = tensor.cpu() .permute((1, 0, 2, 3))
-    im = make_grid(tensor, normalize=True, scale_each=True, nrow=8, padding=2).permute((1, 2, 0))
-    im = (im.data.numpy() * 255.).astype(np.uint8)
-    Image.fromarray(im).save(name + '.jpg')
+    tensor = tensor.cpu().permute((1, 0, 2, 3))
+    im = make_grid(tensor, normalize=True, scale_each=True, nrow=8, padding=2).permute(
+        (1, 2, 0)
+    )
+    im = (im.data.numpy() * 255.0).astype(np.uint8)
+    Image.fromarray(im).save(name + ".jpg")
+
 
 class ISPRS_dataset(torch.utils.data.Dataset):
-    def __init__(self, ids, data_files=DATA_FOLDER, label_files=LABEL_FOLDER,
-                 cache=False, augmentation=True):
+    def __init__(
+        self,
+        ids,
+        data_files=DATA_FOLDER,
+        label_files=LABEL_FOLDER,
+        cache=False,
+        augmentation=True,
+    ):
         super(ISPRS_dataset, self).__init__()
 
         self.augmentation = augmentation
@@ -116,7 +158,7 @@ class ISPRS_dataset(torch.utils.data.Dataset):
         # Sanity check : raise an error if some files do not exist
         for f in self.data_files + self.dsm_files + self.label_files:
             if not os.path.isfile(f):
-                raise KeyError('{} is not a file !'.format(f))
+                raise KeyError("{} is not a file !".format(f))
 
         # Initialize cache dicts
         self.data_cache_ = {}
@@ -124,9 +166,9 @@ class ISPRS_dataset(torch.utils.data.Dataset):
         self.label_cache_ = {}
 
     def __len__(self):
-        if DATASET == 'Potsdam':
+        if DATASET == "Potsdam":
             return 10 * 1000
-        elif DATASET == 'Vaihingen':
+        elif DATASET == "Vaihingen":
             return 10 * 1000
         else:
             return None
@@ -165,16 +207,18 @@ class ISPRS_dataset(torch.utils.data.Dataset):
         else:
             # Data is normalized in [0, 1]
             ## Potsdam IRRG
-            if DATASET == 'Potsdam':
+            if DATASET == "Potsdam":
                 ## RGB
-                data = io.imread(self.data_files[random_idx])[:, :, :3].transpose((2, 0, 1))
+                data = io.imread(self.data_files[random_idx])[:, :, :3].transpose(
+                    (2, 0, 1)
+                )
                 ## IRRG
                 # data = io.imread(self.data_files[random_idx])[:, :, (3, 0, 1, 2)][:, :, :3].transpose((2, 0, 1))
-                data = 1 / 255 * np.asarray(data, dtype='float32')
+                data = 1 / 255 * np.asarray(data, dtype="float32")
             else:
-            ## Vaihingen IRRG
+                ## Vaihingen IRRG
                 data = io.imread(self.data_files[random_idx])
-                data = 1 / 255 * np.asarray(data.transpose((2, 0, 1)), dtype='float32')
+                data = 1 / 255 * np.asarray(data.transpose((2, 0, 1)), dtype="float32")
             if self.cache:
                 self.data_cache_[random_idx] = data
 
@@ -182,7 +226,7 @@ class ISPRS_dataset(torch.utils.data.Dataset):
             dsm = self.dsm_cache_[random_idx]
         else:
             # DSM is normalized in [0, 1]
-            dsm = np.asarray(io.imread(self.dsm_files[random_idx]), dtype='float32')
+            dsm = np.asarray(io.imread(self.dsm_files[random_idx]), dtype="float32")
             min = np.min(dsm)
             max = np.max(dsm)
             dsm = (dsm - min) / (max - min)
@@ -193,7 +237,10 @@ class ISPRS_dataset(torch.utils.data.Dataset):
             label = self.label_cache_[random_idx]
         else:
             # Labels are converted from RGB to their numeric values
-            label = np.asarray(convert_from_color(io.imread(self.label_files[random_idx])), dtype='int64')
+            label = np.asarray(
+                convert_from_color(io.imread(self.label_files[random_idx])),
+                dtype="int64",
+            )
             if self.cache:
                 self.label_cache_[random_idx] = label
 
@@ -207,10 +254,13 @@ class ISPRS_dataset(torch.utils.data.Dataset):
         data_p, dsm_p, label_p = self.data_augmentation(data_p, dsm_p, label_p)
 
         # Return the torch.Tensor values
-        return (torch.from_numpy(data_p),
-                torch.from_numpy(dsm_p),
-                torch.from_numpy(label_p))
-        
+        return (
+            torch.from_numpy(data_p),
+            torch.from_numpy(dsm_p),
+            torch.from_numpy(label_p),
+        )
+
+
 ## We load one tile from the dataset and we display it
 # img = io.imread('./ISPRS_dataset/Vaihingen/top/top_mosaic_09cm_area11.tif')
 # fig = plt.figure()
@@ -230,8 +280,9 @@ class ISPRS_dataset(torch.utils.data.Dataset):
 
 # Utils
 
+
 def get_random_pos(img, window_shape):
-    """ Extract of 2D random patch of shape window_shape in the image """
+    """Extract of 2D random patch of shape window_shape in the image"""
     w, h = window_shape
     W, H = img.shape[-2:]
     x1 = random.randint(0, W - w - 1)
@@ -242,7 +293,7 @@ def get_random_pos(img, window_shape):
 
 
 def CrossEntropy2d(input, target, weight=None, size_average=True):
-    """ 2D version of the cross entropy loss """
+    """2D version of the cross entropy loss"""
     dim = input.dim()
     if dim == 2:
         return F.cross_entropy(input, target, weight, size_average)
@@ -253,7 +304,7 @@ def CrossEntropy2d(input, target, weight=None, size_average=True):
         target = target.view(-1)
         return F.cross_entropy(output, target, weight, size_average)
     else:
-        raise ValueError('Expected 2 or 4 dimensions (got {})'.format(dim))
+        raise ValueError("Expected 2 or 4 dimensions (got {})".format(dim))
 
 
 def accuracy(input, target):
@@ -261,7 +312,7 @@ def accuracy(input, target):
 
 
 def sliding_window(top, step=10, window_size=(20, 20)):
-    """ Slide a window_shape window across the image with a stride of step """
+    """Slide a window_shape window across the image with a stride of step"""
     for x in range(0, top.shape[0], step):
         if x + window_size[0] > top.shape[0]:
             x = top.shape[0] - window_size[0]
@@ -272,7 +323,7 @@ def sliding_window(top, step=10, window_size=(20, 20)):
 
 
 def count_sliding_window(top, step=10, window_size=(20, 20)):
-    """ Count the number of windows in an image """
+    """Count the number of windows in an image"""
     c = 0
     for x in range(0, top.shape[0], step):
         if x + window_size[0] > top.shape[0]:
@@ -285,7 +336,7 @@ def count_sliding_window(top, step=10, window_size=(20, 20)):
 
 
 def grouper(n, iterable):
-    """ Browse an iterator by chunk of n elements """
+    """Browse an iterator by chunk of n elements"""
     it = iter(iterable)
     while True:
         chunk = tuple(itertools.islice(it, n))
@@ -295,10 +346,7 @@ def grouper(n, iterable):
 
 
 def metrics(predictions, gts, label_values=LABELS):
-    cm = confusion_matrix(
-        gts,
-        predictions,
-        labels=range(len(label_values)))
+    cm = confusion_matrix(gts, predictions, labels=range(len(label_values)))
 
     print("Confusion matrix :")
     print(cm)
@@ -318,14 +366,14 @@ def metrics(predictions, gts, label_values=LABELS):
     F1Score = np.zeros(len(label_values))
     for i in range(len(label_values)):
         try:
-            F1Score[i] = 2. * cm[i, i] / (np.sum(cm[i, :]) + np.sum(cm[:, i]))
+            F1Score[i] = 2.0 * cm[i, i] / (np.sum(cm[i, :]) + np.sum(cm[:, i]))
         except:
             # Ignore exception if there is no element in class i for test set
             pass
     print("F1Score :")
     for l_id, score in enumerate(F1Score):
         print("%s: %.4f" % (label_values[l_id], score))
-    print('mean F1Score: %.4f' % (np.nanmean(F1Score[:5])))
+    print("mean F1Score: %.4f" % (np.nanmean(F1Score[:5])))
     print("---")
 
     # Compute kappa coefficient
@@ -333,13 +381,13 @@ def metrics(predictions, gts, label_values=LABELS):
     pa = np.trace(cm) / float(total)
     pe = np.sum(np.sum(cm, axis=0) * np.sum(cm, axis=1)) / float(total * total)
     kappa = (pa - pe) / (1 - pe)
-    print("Kappa: %.4f" %(kappa))
+    print("Kappa: %.4f" % (kappa))
 
     # Compute MIoU coefficient
     MIoU = np.diag(cm) / (np.sum(cm, axis=1) + np.sum(cm, axis=0) - np.diag(cm))
     print(MIoU)
     MIoU = np.nanmean(MIoU[:5])
-    print('mean MIoU: %.4f' % (MIoU))
+    print("mean MIoU: %.4f" % (MIoU))
     print("---")
 
     return MIoU
